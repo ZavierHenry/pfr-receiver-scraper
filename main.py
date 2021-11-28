e9d09ed155d34647cd9ecf45edece3e209b97e8d
Z <bigz.henry63@gmail.com>
2021-11-28 02:13:30 -0600

    Added parsing pro football table rows

--- main.py
+++ main.py
@@ -5,16 +5,19 @@ import requests
 def load_abbreviations(filename):
     pass
 
+def parse_row(row):
+    return { result.attrs['data-stat']: result.string for result in row.find_all('td') }
+
 def parse_page(abbreviation):
     url = f'https://www.pro-football-reference.com/teams/{abbreviation}/career-receiving.htm'
     resp = requests.get(url)
     soup = BeautifulSoup(resp.content, 'html.parser')
-    return [ x.string for x in soup.find_all(attrs={"data-stat": "player"}) ]
+    rows = ( item.parent for item in soup.find_all(lambda tag: tag.has_attr('csk')) )
+    return map(parse_row, rows)
 
 
 if __name__ == "__main__":
     abbreviations_filename = "abbreviations.csv"
     result_filename = "results.csv"
     results = parse_page('chi')
-    for result in results:
-        print(result)
+    print(sorted(results, reverse=True, key=lambda x: int(x['rec_yds']))[0])
